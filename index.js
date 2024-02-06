374499c14c75d59908a1bcb8c14ff2740d9f1cd4
Dawid Chawrona <76160943+chawrona@users.noreply.github.com>
2024-02-06 02:27:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+fetch("img/")
+  .then(response => response.text())
+  .then(data => {
+    // Utwórz tymczasowy element HTML do analizy zawartości
+    const tempElement = document.createElement("div");
+    tempElement.innerHTML = data;
 
-    fetch("img/")
-      .then(response => response.text())
-      .then(data => {
-        // Utwórz tymczasowy element HTML do analizy zawartości
-        const tempElement = document.createElement("div");
-        tempElement.innerHTML = data;
-  
-        // Znajdź wszystkie elementy img (zdjęcia) w folderze
-        const imageElements = tempElement.querySelectorAll("img");
-  
-        // Wyświetl nazwy zdjęć w konsoli
-        imageElements.forEach(img => {
-          console.log(img.src.split('/').pop());
-        });
-      })
+    // Znajdź wszystkie elementy img (zdjęcia) w folderze
+    const imageElements = tempElement.querySelectorAll("img");
 
+    // Dodaj każde zdjęcie do elementu body
+    imageElements.forEach(img => {
+      // Utwórz nowy element img
+      const newImgElement = document.createElement("img");
 
-  
+      // Ustaw atrybuty src i alt dla nowego elementu img
+      newImgElement.src = img.src;
+      newImgElement.alt = img.alt;
+
+      // Dodaj nowy element img do elementu body
+      document.body.appendChild(newImgElement);
+    });
+  });
